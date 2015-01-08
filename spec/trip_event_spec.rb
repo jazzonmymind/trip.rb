a2d42e2948993041f1581344c629c6305ef46edb
strcmp <strcmp@singletonclass.com>
2015-01-08 20:00:44 +0000

    add Trip::Event spec, covers Trip::Event#module so far

---
+++ spec/trip_event_spec.rb
@@ -0,0 +1,16 @@
+require_relative 'setup'
+describe Trip::Event do
+  class Y
+    def self.run
+      # ..
+    end
+  end
+
+  describe "#module" do
+    it "returns a Module" do
+      trip = Trip.new { Y.run }
+      event = trip.start
+      assert_equal Y, event.module
+    end
+  end
+end
