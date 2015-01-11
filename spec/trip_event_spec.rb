68b4469118f4b1703b0bfa33ce61e96e38be645f
strcmp <strcmp@singletonclass.com>
2015-01-11 02:22:41 +0000

    add tests for all methods on Trip::Event

--- spec/trip_event_spec.rb
+++ spec/trip_event_spec.rb
@@ -6,12 +6,51 @@ describe Trip::Event do
     end
   end
 
+  let(:trip) do
+    Trip.new { Y.run }
+  end
+
+  let(:event) do
+    trip.start
+  end
+
+  after do
+    trip.stop
+  end
+
   describe "#module" do
     it "returns a Module" do
-      trip = Trip.new { Y.run }
-      event = trip.start
       assert_equal Y, event.module
-      trip.stop
+    end
+  end
+
+  describe "#file" do
+    it "returns __FILE__ from binding" do
+      assert_equal __FILE__, event.file
+    end
+  end
+
+  describe "#lineno" do
+    it "returns __LINE__ from binding" do
+      assert_equal 4, event.lineno
+    end
+  end
+
+  describe "#method" do
+    it "returns __method__ from binding" do
+      assert_equal :run, event.method
+    end
+  end
+
+  describe "#binding" do
+    it "returns a binding" do
+      assert_instance_of Binding, event.binding
+    end
+  end
+
+  describe "#__binding__" do
+    it "returns a binding for instance of Trip::Event" do
+      assert_equal true, Trip::Event === event.__binding__.eval('self')
     end
   end
 end
